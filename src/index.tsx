79374463d3953eb80a34daa5090d352236ba4a1d
SerjRoman <sergeyroman26@gmail.com>
2024-11-20 19:28:59 +0000

    Product component added

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import {createRoot} from 'react-dom/client'
 import { App } from './App';
 
 
-
 const rootElement = document.getElementById('root') as HTMLElement
 const root = createRoot(rootElement)
 
