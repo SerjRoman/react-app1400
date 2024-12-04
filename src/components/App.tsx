416d67d94af0a2b51f231b51c362aef869396d21
SerjRoman <sergeyroman26@gmail.com>
2024-12-04 20:43:40 +0000

    App Routing added

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,28 @@
 import { ProductsList } from "./ProductsList/ProductsList"
 import { Layout } from "./Layout/Layout"
 import { Header } from "./Header/Header"
+import { Main} from './Main/Main'
 import { Footer } from "./Footer/Footer"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { ProductPage } from "./ProductPage/ProductPage"
+
 
 export function App(){
     return(
         <div>
-            <Layout>
-                <Header></Header>
-                <ProductsList></ProductsList>
-                <Footer></Footer>
-            </Layout>
+            <BrowserRouter>
+                <Routes>
+
+                    <Route path="/" element={<Layout></Layout>}>
+                        <Route path="/products" element={<ProductsList></ProductsList>}></Route>
+                        <Route path="/product/:id" element={<ProductPage></ProductPage>}></Route>
+                    </Route>
+                    {/* <Route path="/" element={<Layout></Layout>}> */}
+                    
+                    {/* </Route> */}
+                    
+                </Routes>
+            </BrowserRouter>
         </div>
     )
 }
