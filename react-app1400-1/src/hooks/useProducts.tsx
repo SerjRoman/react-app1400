793fc2fc3d50c49a44938bcc02bf765c0a365b78
DanilKoshnarev <146003208+DanilKoshnarev@users.noreply.github.com>
2025-01-11 23:06:18 +0200

    Update useProducts.tsx

--- react-app1400-1/src/hooks/useProducts.tsx
+++ react-app1400-1/src/hooks/useProducts.tsx
@@ -1,37 +1,40 @@
 import { useEffect, useState } from "react"
 
+// Интерфейс для продукта
 export interface IProduct{
-    id: number,
-    title: string,
-    description: string,
-    price: number,
-    image: string,
-    category: string,
+    id: number, // Идентификатор продукта
+    title: string, // Название продукта
+    description: string, // Описание продукта
+    price: number, // Цена продукта
+    image: string, // Ссылка на изображение продукта
+    category: string, // Категория продукта
 }
 
+// Хук для получения продуктов
 export function useProducts(){
-    const [products, setProducts] = useState<IProduct[]>([])
-    const [isLoading, setIsLoading] = useState<boolean>(false)
-    const [error, setError] = useState<string>()
+    const [products, setProducts] = useState<IProduct[]>([]) // Состояние для хранения продуктов
+    const [isLoading, setIsLoading] = useState<boolean>(false) // Состояние для отслеживания загрузки
+    const [error, setError] = useState<string>() // Состояние для хранения ошибок
+
     useEffect(()=>{
+        // Асинхронная функция для получения продуктов
         async function getProducts(){
             try{
-                setIsLoading(true)
-                const response = await fetch('https://fakestoreapi.com/products')
-                const products = await response.json()
-                setProducts(products)
+                setIsLoading(true) // Устанавливаем состояние загрузки в true
+                const response = await fetch('https://fakestoreapi.com/products') // Запрос к API
+                const products = await response.json() // Преобразуем ответ в JSON
+                setProducts(products) // Сохраняем продукты в состояние
             }
             catch(error){
-                const err = error instanceof Error ? error.message : undefined
-                setError(`${err}`)
+                const err = error instanceof Error ? error.message : undefined // Обработка ошибок
+                setError(`${err}`) // Сохраняем ошибку в состояние
             }
             finally{
-                setIsLoading(false)
+                setIsLoading(false) // Устанавливаем состояние загрузки в false
             }
-            
         }
-        getProducts()
-        
+        getProducts() // Вызываем функцию получения продуктов
     },[])
-    return {products: products, isLoading: isLoading, error: error}
+
+    return {products: products, isLoading: isLoading, error: error} // Возвращаем состояние
 }
