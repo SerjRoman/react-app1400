0536d290607cc4e77ef59dfb4809c9a39a1a9053
Misha Fatuev <mishafatuev@gmail.com>
2025-01-15 12:06:27 +0200

    comments

--- src/shared/Modal/Modal.tsx
+++ src/shared/Modal/Modal.tsx
@@ -9,24 +9,23 @@ interface IModalProps {
     container?: Element
 }
 
+export function Modal(props: IModalProps){ 
 
-
-export function Modal(props: IModalProps){
+    // Деструктуризируем props в children, allowModalCloseOutside, onClose и container
     let {children, allowModalCloseOutside, onClose, container=document.body} = props
 
+    // Функция которая проверяет, на что мы нажали при открытом модальном окне, если мы нажали на что-то, что не связано с модальным окном, то окно закроется
     function handleClickOutside(event: MouseEvent){
-    
         console.log(event.target)
         console.log(modalRef.current)
-
         if (modalRef.current !== event.target && !modalRef.current?.contains(event.target as Node)){
             // setIsModalOpened(false)
             onClose()
-
         }
-        
     }
 
+    // Вмонтирует проверку, открыто ли модальное окно, если да, то к документу добавляется addEventListener
+    // который при клике на что либо вызывает функцию handleClickOutside, также  useEffect возвращает стрелочную функцию которая убирает addEventListener с документа
     useEffect(() => {
         if (!allowModalCloseOutside){
             return
@@ -38,13 +37,12 @@ export function Modal(props: IModalProps){
         }
     }, [])
 
-    
-
+    // Получаем "ссылку" на елемент модального окна
     const modalRef = useRef<HTMLDivElement | null>(null)
 
+    // Возвращает портал с модальным окном, внутри которого находится children
     return createPortal(
         <div ref={modalRef} className="modal">{children}</div>,
         container
     )
-        // <div ref={modalRef} className="modal">{children}</div>    
 }
