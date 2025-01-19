068f958713a2f7627d464063d126caedbf74d911
SerjRoman <sergeyroman26@gmail.com>
2025-01-19 13:54:23 +0000

    Cartcontext created

--- src/shared/App.tsx
+++ src/shared/App.tsx
@@ -3,24 +3,35 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { ProductListPage } from "../pages/ProductListPage/ProductListPage"
 import { ProductPage } from "../pages/ProductPage/ProductPage"
 import { MainPage } from "../pages/MainPage/MainPage"
+import { createContext } from "react"
 
+interface ICartItem{
+    name: string;
+    price: number
+    // addItem: () =>
+}
+
+const initialValue: ICartItem[] = []
+const cartContext = createContext< ICartItem[] >(initialValue)
 
 export function App(){
     return(
         <div>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Layout></Layout>}>
-                        <Route path="/" element={<MainPage></MainPage>}></Route>
-                        <Route path="/products" element={<ProductListPage></ProductListPage>}></Route>
-                        <Route path="/product/:id" element={<ProductPage></ProductPage>}></Route>
-                    </Route>
-                    {/* <Route path="/" element={<Layout></Layout>}> */}
-                    
-                    {/* </Route> */}
-                    
-                </Routes>
-            </BrowserRouter>
+            <cartContext.Provider value={[{name:"product1",price:1423}]}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Layout></Layout>}>
+                            <Route path="/" element={<MainPage></MainPage>}></Route>
+                            <Route path="/products" element={<ProductListPage></ProductListPage>}></Route>
+                            <Route path="/product/:id" element={<ProductPage></ProductPage>}></Route>
+                        </Route>
+                        {/* <Route path="/" element={<Layout></Layout>}> */}
+                        
+                        {/* </Route> */}
+                        
+                    </Routes>
+                </BrowserRouter>
+            </cartContext.Provider>
         </div>
     )
 }
