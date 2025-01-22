c6dbf4f437dfab8fcde1f4fff9c37706d6c17524
DanilKoshnarev <146003208+DanilKoshnarev@users.noreply.github.com>
2025-01-22 17:36:04 +0200

    Update App.tsx

--- react-app1400-1/src/shared/App.tsx
+++ react-app1400-1/src/shared/App.tsx
@@ -1,26 +1,24 @@
-import { Layout } from "./Layout/Layout"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { ProductListPage } from "../pages/ProductListPage/ProductListPage"
-import { ProductPage } from "../pages/ProductPage/ProductPage"
-import { MainPage } from "../pages/MainPage/MainPage"
+import { Layout } from "./Layout/Layout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ProductListPage } from "../pages/ProductListPage/ProductListPage";
+import { ProductPage } from "../pages/ProductPage/ProductPage";
+import { MainPage } from "../pages/MainPage/MainPage";
+import { FavoritesProvider } from "./FavoritesContext";
 
-
-export function App(){
-    return(
+export function App() {
+    return (
         <div>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Layout></Layout>}>
-                        <Route path="/" element={<MainPage></MainPage>}></Route>
-                        <Route path="/products" element={<ProductListPage></ProductListPage>}></Route>
-                        <Route path="/product/:id" element={<ProductPage></ProductPage>}></Route>
-                    </Route>
-                    {/* <Route path="/" element={<Layout></Layout>}> */}
-                    
-                    {/* </Route> */}
-                    
-                </Routes>
-            </BrowserRouter>
+            <FavoritesProvider>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Layout />}>
+                            <Route path="/" element={<MainPage />} />
+                            <Route path="/products" element={<ProductListPage />} />
+                            <Route path="/product/:id" element={<ProductPage />} />
+                        </Route>
+                    </Routes>
+                </BrowserRouter>
+            </FavoritesProvider>
         </div>
-    )
+    );
 }
