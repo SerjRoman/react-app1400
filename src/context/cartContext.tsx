3342cdbfed62af9539165a4c720c1230d4539b5b
Misha Fatuev <mishafatuev@gmail.com>
2025-02-01 20:54:11 +0200

    added css styles for cartPage

--- src/context/cartContext.tsx
+++ src/context/cartContext.tsx
@@ -28,7 +28,31 @@ interface ICartContextProviderProps{
 
 export function CartContextProvider(props: ICartContextProviderProps) {
     const { children } = props
-    const [cartProducts, setCartProducts] = useState<IProduct[]>([])
+    const [cartProducts, setCartProducts] = useState<IProduct[]>([{
+        "id": 1,
+        "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+        "price": 109.95,
+        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        "category": "men's clothing",
+        "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+      },
+      {
+        "id": 2,
+        "title": "Mens Casual Premium Slim Fit T-Shirts ",
+        "price": 22.3,
+        "description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+        "category": "men's clothing",
+        "image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+      },
+      {
+        "id": 3,
+        "title": "Mens Cotton Jacket",
+        "price": 55.99,
+        "description": "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
+        "category": "men's clothing",
+        "image": "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+      }
+    ])
 
     function addToCart(product: IProduct){
         let array = [...cartProducts, product]
@@ -43,16 +67,10 @@ export function CartContextProvider(props: ICartContextProviderProps) {
     }
 
     function isInCart(id: number) {
-        // id = 4
-        // [{id: 1}, {id: 2}, {id: 3},{id: 4}]
-        // {id: 1} -> 1 !== 4 -> true
-        // {id: 2} -> 2 !== 4 -> true
-        // {id: 3} -> 3 !== 4 -> true
-        // {id: 4} -> 4 !== 4 -> false
         const result = cartProducts.some((product)=>{
             return product.id === id 
         });
-        return result //😲
+        return result
     }
 
     return(
--- src/shared/ProductsList/ProductsList.tsx
+++ src/shared/ProductsList/ProductsList.tsx
@@ -6,16 +6,6 @@ import { Vortex } from 'react-loader-spinner'
 import { useProducts } from "../../hooks/useProducts"
 import { useCategories } from "../../hooks/useCategories"
 
-// const products = [
-//     {id: 0, category: 'Chat-Bot', title: 'Daniel', image: 'https://th-thumbnailer.cdn-si-edu.com/faJoWtc8qjIHuCadMQ2MKjt6xmo=/1000x750/filters:no_upscale()/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/6a/fa/6afa4efa-3f5a-4ea7-90ea-e47173217d59/42-29316901.jpg', price: -15},
-//     {id: 1, category: 'Floppa', title: 'Ilia Bulkin', image: 'https://s7d2.scene7.com/is/image/TWCNews/AP21286725279031_crop?wid=767&hei=431&$wide-bg$', price: -30},
-//     {id: 2, category: 'Cats', title: 'Vanya', image: 'https://www.thewildlifediaries.com/wp-content/uploads/2018/11/Snapseed-1-768x512.jpg', price: -45},
-//     {id: 3, category: 'Cats', title: 'Фатуев Михайло', image: 'https://www.thewildlifediaries.com/wp-content/uploads/2018/04/Siberian-tiger-768x512.jpg', price: 52},
-//     {id: 4, category: 'Chat-Bot', title: 'Daniel4', image: 'https://www.thewildlifediaries.com/wp-content/uploads/2020/01/leopard-walking-768x512.jpg', price: -60}
-// ]
-
-
-
 export function ProductsList(){
     const {products, isLoading, error} = useProducts()
 
@@ -35,16 +25,6 @@ export function ProductsList(){
         console.log(selectedCategory)
     }, [selectedCategory, products])
 
-    // useEffect(()=>{
-    //     async function getCategories(){
-    //         const response = await fetch('https://fakestoreapi.com/products/categories')
-    //         const categories = await response.json()
-    //         setFilteredProducts(categories)
-    //     }
-    //     getCategories()
-    // },[])
-
-
     return <div className="product-list">
         <div className="select-category">
         <select onChange={(event)=>{
@@ -59,10 +39,6 @@ export function ProductsList(){
         </div>
         <div className="products">
             { isLoading === false ? !error ? filteredProducts.map( (product) => {
-                // key - уникальный ключ используется при рендере массивов
-                // нужен для идентифицирования reactом элементов которые отображаются
-                    // <img src="" alt="" />
-                
                 return <Product key = {product.id} id={product.id} name = {product.title} price = {product.price} img = {product.image}></Product>
             }) : (<div>{error}</div>) : (<div className="vortex"><Vortex
                 visible={true}
