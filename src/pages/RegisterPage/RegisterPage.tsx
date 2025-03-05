bac4d89ab643000048fe1f7c8768795062c347c2
SerjRoman <sergeyroman26@gmail.com>
2025-03-05 19:40:23 +0000

    Login and register page created

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -1,12 +1,14 @@
 import { useForm } from "react-hook-form"
 
 interface IForm {
+    username: string,
     email: string,
     password: string,
+    image: string,
 }
 
 // yup validation
-export function LoginPage (){
+export function RegisterPage (){
     // Пишем скобки, которые отвечают за то, что мы будем деструктуризировать
     const {register: register, handleSubmit, formState} = useForm <IForm>({
         mode: 'onSubmit'
@@ -14,7 +16,7 @@ export function LoginPage (){
     
     function onSubmit(data: IForm){
         console.log(data)
-        fetch('http://localhost:8000/api/user/login', { 
+        fetch('http://localhost:8000/api/user/register', { 
             method: 'POST',
             headers: { 'Content-Type': 'application/json'},
             body: JSON.stringify(data)
@@ -25,6 +27,13 @@ export function LoginPage (){
     return(
         <div>
             <form onSubmit={handleSubmit(onSubmit)}>
+                <input type="text" {...register('username', {
+                    required: {value: true, message: 'Field is required'}, 
+                    minLength: {value: 7, message: 'This field should be more than 7 symbols'}, 
+                    maxLength: {value: 100, message: 'This field should be less than 100 symbols'}, })} />
+
+                <p>{formState.errors.username?.message}</p>
+
                 <input type="text" {...register('email', {
                     required: {value: true, message: 'Field is required'}, 
                     minLength: {value: 7, message: 'This field should be more than 7 symbols'}, 
@@ -38,6 +47,10 @@ export function LoginPage (){
                     maxLength: {value: 100, message: 'This field should be less than 100 symbols'}, })} />
                 
                 <p>{formState.errors.password?.message}</p>
+                <input type="url" {...register('image', {
+                    required: {value: true, message: 'Field is required'}})} />
+
+                <p>{formState.errors.image?.message}</p>
                 <button type="submit">Submit</button>
             </form>
         </div>
