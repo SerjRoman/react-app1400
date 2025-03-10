049e623c2807ac4bc88fe11d85f2832c72e4759b
Ilia Bulkin <bulkin20088@gmail.com>
2025-03-10 23:33:23 +0200

    [09.03.2025] homework, added comments on file userContext about code functional, I hope my english is not very bad lol

--- src/context/userContext.tsx
+++ src/context/userContext.tsx
@@ -1,6 +1,7 @@
 import { createContext, useContext, ReactNode, useState } from "react"
 import { Response } from '../shared/types/response'
 
+// оределяет структуру объекта пользователя
 interface IUser {
     email: string
     username: string
@@ -8,6 +9,7 @@ interface IUser {
     role: string
 }
 
+// определяет какие данные и функции будут доступны в контексте
 interface IUserContext{
     user: IUser | null
     login: (email: string, password: string) => void
@@ -15,6 +17,7 @@ interface IUserContext{
     isAuthenticated: () => boolean
 }
 
+// создаем пустой контекст
 const initialValue: IUserContext = {
     user: null,
     login: (email: string, password: string) => {},
@@ -23,6 +26,7 @@ const initialValue: IUserContext = {
 }
 const userContext = createContext<IUserContext>(initialValue)
 
+// хук для доступа к контексту
 export function useUserContext(){
     return useContext(userContext)
 }
@@ -31,11 +35,13 @@ interface IUserContextProviderProps{
     children?: ReactNode
 }
 
+// провайдер контекста
 export function UserContextProvider(props: IUserContextProviderProps){
     const [user, setUser] = useState<IUser | null>(null)
 
     async function getData(token: string){
         try{
+            // делает запрос в наше апи, и если с токеном всё норм, то обновляет состояние юзера
             const response = await fetch('http://localhost:8000/api/user/me', {
                 headers: {'Authorization': `Bearer ${token}`}
             })
@@ -44,6 +50,7 @@ export function UserContextProvider(props: IUserContextProviderProps){
                 console.log(result.message) 
                 return
             }
+            // обновляет вот тут 
             setUser(result.data)
         } catch(error){
 
@@ -52,7 +59,9 @@ export function UserContextProvider(props: IUserContextProviderProps){
 
     async function login(email: string, password: string){
         try{
-            const response = await fetch('http://localhost:8000/api/user/login', { 
+            // отпрявляет имеил и пароль на сервер, после чего вызывает предыдущую функцию getData
+            const response = await fetch('http://localhost:8000/api/user/login', {
+                // отправляет вот тут 
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json'},
                 body: JSON.stringify({'email': email, 'password': password})
@@ -62,6 +71,7 @@ export function UserContextProvider(props: IUserContextProviderProps){
                 console.log(result.message)
                 return
             }
+            // вызывает отута
             getData(result.data)
         } catch(error){
 
@@ -70,6 +80,7 @@ export function UserContextProvider(props: IUserContextProviderProps){
     
     async function register(email: string, username: string, image: string, password: string){
         try {
+            // тоже отправляет данные на сервак, но плюсом юзернейм и аватарку пользователя 
             const response = await fetch('http://localhost:8000/api/user/register', { 
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json'},
@@ -77,6 +88,7 @@ export function UserContextProvider(props: IUserContextProviderProps){
             })
 
             const result: Response<string> = await response.json();
+            // проверяет статус результата, если ошибка, то ваи плоха усё
             if (result.status === 'error'){
                 console.log(result.message);
                 return;
@@ -87,7 +99,7 @@ export function UserContextProvider(props: IUserContextProviderProps){
 
         }
     }
-
+    // ретурнит данные если всё норм
     return <userContext.Provider 
     value={{
         user: user,
